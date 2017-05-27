a8465a166da41292b35321a56c906786f47dae3b
孤独的蔬菜 <shuai19970217@163.com>
2017-05-27 10:00:23 +0800

    常用类

--- 5.26_lx/Program.cs
+++ 5.26_lx/Program.cs
@@ -10,6 +10,50 @@ namespace _5._26_lx
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("请输入一个最小值：");
+            int min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("请输入一个最大值：");
+            int max = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("请输入你要获取元素的个数：");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            GetShow(min,max,count);
+            Console.ReadKey();
+
+        }
+
+        //private static void GetShow(int min, int max, int count)
+        //{
+        //    int[] num = new int[count];
+
+        //    for (int i = 0; i < num.Length; i++)
+        //    {
+
+        //    }
+
+        //}
+
+        private static void GetShow(int min, int max, int count)
+        {
+            List<int> list = new List<int>();
+
+            Random random = new Random();
+
+            for (int i = 0; i < count; i++)
+            {
+                int temp = random.Next(min, max);
+                if (!list.Contains(temp))
+                {
+                    list.Add(temp);
+                }
+
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(" "+list[i]);
+            }
+
         }
     }
 }
---
+++ NewDateTime/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DateTime dt = DateTime.Now;
+            //Console.WriteLine(dt.ToShortDateString());
+            //Console.WriteLine(dt.ToShortTimeString());
+            //Console.WriteLine(dt.ToString());
+
+
+            //string str = "2014/3/24";
+            //DateTime d = Convert.ToDateTime(str);
+            //Console.WriteLine(d.ToShortDateString());
+
+
+            #region  Array
+
+          
+            Array array = Array.CreateInstance(typeof(int),10);
+
+            for (int i = 0; i <array.Length; i++)
+            {
+                array.SetValue(i+1,i);
+            }
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array.GetValue(i).ToString()+"\t");
+
+            }
+
+
+       
+
+            Console.ReadKey();
+
+            #endregion
+        }
+    }
+}
